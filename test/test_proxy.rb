89361fedb35df8dd367860126cc1dbf837af6da7
Tim Pease <tim.pease@gmail.com>
2011-02-07 09:55:32 -0700

    adding some tests to be filled out later

---
+++ test/test_proxy.rb
@@ -0,0 +1,23 @@
+
+require File.expand_path('../setup', __FILE__)
+
+module TestLogging
+
+  class TestProxy < Test::Unit::TestCase
+    include LoggingTestCase
+
+    def test_initialize
+      flunk 'needs to be tested'
+    end
+
+    def test_method_logging
+      flunk 'needs to be tested'
+    end
+
+    def test_custom_method_logging
+      flunk 'needs to be tested'
+    end
+
+  end  # TestProxy
+end  # TestLogging
+
