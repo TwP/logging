69d8221980251e64c93def344c2428891026c623
Tim Pease <tim.pease@gmail.com>
2011-08-18 16:02:51 -0600

    Support for rspec 2

--- lib/spec/logging_helper.rb
+++ lib/spec/logging_helper.rb
@@ -1,34 +1,10 @@
 
-module Spec
-  module LoggingHelper
+require File.expand_path('../../rspec/logging_helper', __FILE__)
+Spec::LoggingHelper = RSpec::LoggingHelper
 
-    # Capture log messages from the Logging framework and make them
-    # available via a @log_output instance variable. The @log_output
-    # supports a readline method to access the log messags.
-    #
-    def capture_log_messages( opts = {} )
-      from = opts.getopt(:from, 'root')
-      to = opts.getopt(:to, '__rspec__')
-      exclusive = opts.getopt(:exclusive, true)
+if defined?  Spec::Runner::Configuration
+  class Spec::Runner::Configuration
+    include Spec::LoggingHelper
+  end
+end
 
-      appender = Logging::Appenders[to] || Logging::Appenders::StringIo.new(to)
-      logger = Logging::Logger[from]
-      if exclusive
-        logger.appenders = appender
-      else
-        logger.add_appenders(appender)
-      end
-
-      before(:all) do
-        @log_output = Logging::Appenders[to]
-      end
-
-      before(:each) do
-        @log_output.reset
-      end
-    end
-
-  end  # module LoggingHelper
-end  # module Spec
-
-# EOF
