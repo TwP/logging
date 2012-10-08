47be512f98b4d680192a71497890e4d442dc14af
Tim Pease <tim.pease@gmail.com>
2012-10-07 21:50:37 -0600

    Fixing up examples.

--- examples/custom_log_levels.rb
+++ examples/custom_log_levels.rb
@@ -1,48 +1,53 @@
 # :stopdoc:
 #
-# It's useful to define custom log levels that denote success, or otherwise meaningful events that happen to not be negative (more than 50% of the levels
-# are given to warn, error, fail - quite a pessimistic view of one's application's
-# chances of success, no?  ;-) )
+# It's useful to define custom log levels that denote success, or otherwise
+# meaningful events that happen to not be negative (more than 50% of the
+# levels are given to warn, error, fail - quite a pessimistic view of one's
+# application's chances of success, no?  ;-) )
 #
-# Here, we define two new levels, 'happy' and 'success' and make them soothing colours.
+# Here, we define two new levels, 'happy' and 'success' and make them soothing
+# colours.
+#
 
-require 'logging'
+  require 'logging'
 
-# https://github.com/TwP/logging/blob/master/lib/logging.rb#L250-285
-# The levels run from lowest level to highest level.
-# I'm still pessimistic of my programs enough so that my success levels are lower than my failure levels...
-Logging.init :debug, :info, :happy, :warn, :success, :error, :fatal
+  # https://github.com/TwP/logging/blob/master/lib/logging.rb#L250-285
+  # The levels run from lowest level to highest level.
 
-Logging.color_scheme( 'soothing_ish',
-  :levels => {
-		:info  => :cyan,
-    :happy => :green,
-    :warn  => :yellow,
-    :success => [:blue],
-    :error => :red,
-    :fatal => [:white, :on_red]
-  },
-  :date => :cyan,
-  :logger => :cyan,
-  :message => :orange
-)
+  Logging.init :debug, :info, :happy, :warn, :success, :error, :fatal
 
-Logging.appenders.stdout(
-  'stdout',
-  :layout => Logging.layouts.pattern(
-    :pattern => '[%d] %-5l %c: %m\n',
-    :color_scheme => 'soothing_ish'
+  Logging.color_scheme( 'soothing_ish',
+    :levels => {
+      :info  => :cyan,
+      :happy => :green,
+      :warn  => :yellow,
+      :success => [:blue],
+      :error => :red,
+      :fatal => [:white, :on_red]
+    },
+    :date => :cyan,
+    :logger => :cyan,
+    :message => :orange
   )
-)
 
-log = Logging.logger['Soothing::Colors']
-log.add_appenders 'stdout'
-log.level = :debug
+  Logging.appenders.stdout(
+    'stdout',
+    :layout => Logging.layouts.pattern(
+      :pattern => '[%d] %-7l %c: %m\n',
+      :color_scheme => 'soothing_ish'
+    )
+  )
 
-log.debug "a very nice little debug message"
-log.info "things are operating nominally"
-log.happy "What a beautiful day"
-log.warn "this is your last warning"
-log.success "I am INWEENCIBLE!!"
-log.error StandardError.new("something went horribly wrong")
-log.fatal "I Die!"
+  log = Logging.logger['Soothing::Colors']
+  log.add_appenders 'stdout'
+  log.level = :debug
+
+  log.debug   'a very nice little debug message'
+  log.info    'things are operating nominally'
+  log.happy   'What a beautiful day'
+  log.warn    'this is your last warning'
+  log.success 'I am INWEENCIBLE!!'
+  log.error   StandardError.new('something went horribly wrong')
+  log.fatal   'I Die!'
+
+# :startdoc:
