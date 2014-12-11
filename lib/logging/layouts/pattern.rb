5177ac925371bcbd7a7812e2611b17b41ee4d55f
Peter Schrammel <peter.schrammel@gmx.de>
2014-12-11 13:45:38 +0100

    refactoring of the create_format_pattern

--- lib/logging/layouts/pattern.rb
+++ lib/logging/layouts/pattern.rb
@@ -163,8 +163,8 @@ module Logging::Layouts
       'r' => 'Integer((event.time-@created_at)*1000).to_s'.freeze,
       't' => 'Thread.current.object_id.to_s'.freeze,
       'T' => 'Thread.current[:name]'.freeze,
-      'X' => :placeholder,
-      'x' => :placeholder,
+      'X' => lambda do |var_name| "::Logging.mdc['#{var_name}']" end,
+      'x' => lambda do |separator| "::Logging.ndc.context.join('#{separator}')" end,
       '%' => :placeholder
     }.freeze
 
@@ -231,104 +231,150 @@ module Logging::Layouts
     # Layout _pf_ based on the configured format pattern specified by the
     # user.
     #
+
+    class SprintfBag
+      def initialize(color_scheme,color_alias_table, directive_table)
+        @format_string='"'
+        @args=[]
+        @name_map={}
+        @color_scheme=color_scheme
+        @color_alias_table=color_alias_table
+        @directive_table = directive_table
+
+      end
+
+      # @param [String] pattern_name like 'message' or 'time'
+      # @param [String] string the string that needs coloring
+      def add_colored(string,pattern_name,arg=nil)
+        if @color_scheme and !@color_scheme.lines?
+          @format_string << @color_scheme.color(string,@color_alias_table[pattern_name])
+        else
+          @format_string << string
+        end
+        directive=@directive_table[pattern_name]
+        if @directive_table[pattern_name].respond_to?(:call)
+          @args << directive.call(arg)
+        else
+          @args << directive.dup
+        end
+        self
+      end
+
+      def add(str)
+        @format_string << str
+        self
+      end
+      alias :<< add
+
+      def add_arg(arg, modify=false)
+        if modify
+          @args.last << arg
+        else
+          @args << arg
+        end
+        self
+      end
+
+      def add_variable(value,arg="@var")
+        map_name="@map_name_#{@name_map.size}"
+        @name_map[map_name]=value
+        @args << arg.gsub("@var",map_name)
+        self
+      end
+
+      def set_variables(obj)
+        @name_map.each_pair do |name, value|
+          obj.instance_variable_set(name.to_sym, value)
+        end
+      end
+
+      def to_sprintf
+        sprintf = "sprintf("
+        sprintf << @format_string + '"'
+        sprintf << ', ' + @args.join(', ') unless @args.empty?
+        sprintf << ")"
+
+        if @color_scheme and @color_scheme.lines? #generate colors at runtime
+          sprintf = "color_scheme.color(#{sprintf}, ::Logging::LNAMES[event.level])"
+        end
+
+        sprintf
+      end
+
+
+    end
+
     def self.create_format_method( pf )
-      # Create the format(event) method
-      format_string = '"'
-      pattern = pf.pattern.dup
-      color_scheme = pf.color_scheme
-      args = []
-      name_map_count = 0
+      spf_bag=SprintfBag.new(pf.color_scheme,COLOR_ALIAS_TABLE,DIRECTIVE_TABLE)
+      pattern=pf.pattern.dup
 
       while true
         m = DIRECTIVE_RGXP.match(pattern)
-        format_string << m[1] unless m[1].empty?
+        # * $1 is the stuff before directive or "" if not applicable
+        # * $2 is the %#.# match within directive group
+        # * $3 is the directive letter
+        # * $4 is the precision specifier for the logger name
+        # * $5 is the stuff after the directive or "" if not applicable
+
+        spf_bag << m[1] unless m[1].empty?
 
         case m[3]
-        when '%'; format_string << '%%'
-        when 'c'
-          fmt = m[2] + 's'
-          fmt = color_scheme.color(fmt, COLOR_ALIAS_TABLE[m[3]]) if color_scheme and !color_scheme.lines?
-
-          format_string << fmt
-          args << DIRECTIVE_TABLE[m[3]].dup
-          if m[4]
-            precision = Integer(m[4]) rescue nil
-            if precision
-              raise ArgumentError, "logger name precision must be an integer greater than zero: #{precision}" unless precision > 0
-              args.last <<
-                  ".split(::Logging::Repository::PATH_DELIMITER)" \
-                  ".last(#{m[4]}).join(::Logging::Repository::PATH_DELIMITER)"
-            else
-              format_string << "{#{m[4]}}"
+          when '%'; spf_bag << '%%'
+          when 'c'
+            spf_bag.add_colored(m[2]+'s',m[3])
+            if m[4]
+              precision = Integer(m[4]) rescue nil
+              if precision
+                raise ArgumentError, "logger name precision must be an integer greater than zero: #{precision}" unless precision > 0
+                spf_bag.add_arg(".split(::Logging::Repository::PATH_DELIMITER).last(#{m[4]}).join(::Logging::Repository::PATH_DELIMITER)",true)
+              else
+                spf_bag << "{#{m[4]}}"
+              end
             end
-          end
-        when 'l'
-          if color_scheme and color_scheme.levels?
-            name_map = ::Logging::LNAMES.map { |name| color_scheme.color(("#{m[2]}s" % name), name) }
-            var = "@name_map_#{name_map_count}"
-            pf.instance_variable_set(var.to_sym, name_map)
-            name_map_count += 1
+          when 'l' #level is dynamic
+            if pf.color_scheme and pf.color_scheme.levels?
+              name_map = ::Logging::LNAMES.map { |name| pf.color_scheme.color(("#{m[2]}s" % name), name) }
+              spf_bag.add_variable(name_map,"@var[event.level]") << '%s'
+              spf_bag << "{#{m[4]}}" if m[4]
+            else
+              spf_bag << m[2] + 's'
+              spf_bag << "{#{m[4]}}" if m[4]
+              spf_bag.add_arg(DIRECTIVE_TABLE[m[3]])
+            end
+          when 'X'
+            raise ArgumentError, "MDC must have a key reference" unless m[4]
+            spf_bag.add_colored(m[2] + 's',m[3], m[4])
 
-            format_string << '%s'
-            format_string << "{#{m[4]}}" if m[4]
-            args << "#{var}[event.level]"
+          when 'x'
+            separator = m[4].to_s
+            separator = ' ' if separator.empty?
+            spf_bag.add_colored(m[2] + 's',m[3], separator)
+
+          when *DIRECTIVE_TABLE.keys
+            spf_bag.add_colored(m[2] + 's',m[3])
+            spf_bag << "{#{m[4]}}" if m[4]
+          when nil; break
           else
-            format_string << m[2] + 's'
-            format_string << "{#{m[4]}}" if m[4]
-            args << DIRECTIVE_TABLE[m[3]]
-          end
-
-        when 'X'
-          raise ArgumentError, "MDC must have a key reference" unless m[4]
-          fmt = m[2] + 's'
-          fmt = color_scheme.color(fmt, COLOR_ALIAS_TABLE[m[3]]) if color_scheme and !color_scheme.lines?
-
-          format_string << fmt
-          args << "::Logging.mdc['#{m[4]}']"
-
-        when 'x'
-          fmt = m[2] + 's'
-          fmt = color_scheme.color(fmt, COLOR_ALIAS_TABLE[m[3]]) if color_scheme and !color_scheme.lines?
-
-          format_string << fmt
-          separator = m[4].to_s
-          separator = ' ' if separator.empty?
-          args << "::Logging.ndc.context.join('#{separator}')"
-
-        when *DIRECTIVE_TABLE.keys
-          fmt = m[2] + 's'
-          fmt = color_scheme.color(fmt, COLOR_ALIAS_TABLE[m[3]]) if color_scheme and !color_scheme.lines?
-
-          format_string << fmt
-          format_string << "{#{m[4]}}" if m[4]
-          args << DIRECTIVE_TABLE[m[3]]
-
-        when nil; break
-        else
-          raise ArgumentError, "illegal format character - '#{m[3]}'"
+            raise ArgumentError, "illegal format character - '#{m[3]}'"
         end
 
         break if m[5].empty?
         pattern = m[5]
       end
 
-      format_string << '"'
-
-      sprintf = "sprintf("
-      sprintf << format_string
-      sprintf << ', ' + args.join(', ') unless args.empty?
-      sprintf << ")"
-
-      if color_scheme and color_scheme.lines?
-        sprintf = "color_scheme.color(#{sprintf}, ::Logging::LNAMES[event.level])"
-      end
+      sprintf=spf_bag.to_sprintf
 
       code = "undef :format if method_defined? :format\n"
       code << "def format( event )\n#{sprintf}\nend\n"
       ::Logging.log_internal(0) {code}
-
+      spf_bag.set_variables(pf)
       pf._meta_eval(code, __FILE__, __LINE__)
     end
+
+
+
+
+
     # :startdoc:
 
     # call-seq:
