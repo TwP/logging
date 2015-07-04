5f595fc239228756e6a543ee4cf93fadc5f77094
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-07-03 17:31:46 -0700

    updated spec

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -33,8 +33,8 @@ module TestLogging
       assert_nil a1.readline
       assert_nil a2.readline
 
-      log.add(1, nil, 'this should be logged')
-      assert_equal " INFO  A Logger : this should be logged\n", a1.readline
+      log.add(1, nil, 'this should be logged (when used by Rails.logger.extend(ActiveSupport::Logger.broadcast())')
+      assert_equal " INFO  A Logger : this should be logged (when used by Rails.logger.extend(ActiveSupport::Logger.broadcast())\n", a1.readline
       assert_nil a1.readline
       assert_nil a2.readline
 
