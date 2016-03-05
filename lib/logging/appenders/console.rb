0bf7706f6d772ad77e6b28cae8a7ddceb8bc2d1f
Tim Pease <tim.pease@gmail.com>
2016-03-04 20:18:55 -0700

    adding some clever superclass code

--- lib/logging/appenders/console.rb
+++ lib/logging/appenders/console.rb
@@ -1,6 +1,57 @@
 
 module Logging::Appenders
 
+  # This class is provides an Appender base class for writing to the standard IO
+  # stream - STDOUT and STDERR. This class should not be instantiated directly.
+  # The `Stdout` and `Stderr` subclasses should be used.
+  class Console < ::Logging::Appenders::IO
+
+    # call-seq:
+    #    Stdout.new( name = 'stdout' )
+    #    Stderr.new( :layout => layout )
+    #    Stdout.new( name = 'stdout', :level => 'info' )
+    #
+    # Creates a new Stdout/Stderr Appender. The name 'stdout'/'stderr' will be
+    # used unless another is given. Optionally, a layout can be given for the
+    # appender to use (otherwise a basic appender will be created) and a log
+    # level can be specified.
+    #
+    # Options:
+    #
+    #    :layout => the layout to use when formatting log events
+    #    :level  => the level at which to log
+    #
+    def initialize( *args )
+      name = self.class.name.split("::").last.downcase
+      io   = Object.const_get(name.upcase)
+
+      opts = Hash === args.last ? args.pop : {}
+      name = args.shift unless args.empty?
+
+      opts[:encoding] = io.external_encoding if io.respond_to? :external_encoding
+
+      super(name, io, opts)
+    end
+
+  private
+
+    # @override of ::Logging::Appenders::IO
+    def canonical_write( str )
+      return self if @io.nil?
+      str = str.force_encoding(encoding) if encoding && str.encoding != encoding
+      @io.write str # instead of syswrite
+      self
+    rescue StandardError => err
+      handle_internal_error(err)
+    end
+  end
+
+  # This class provides an Appender that can write to STDOUT.
+  Stdout = Class.new(Console)
+
+  # This class provides an Appender that can write to STDERR.
+  Stderr = Class.new(Console)
+
   # Accessor / Factory for the Stdout appender.
   #
   def self.stdout( *args )
@@ -10,49 +61,6 @@ module Logging::Appenders
     ::Logging::Appenders::Stdout.new(*args)
   end
 
-  # This class provides an Appender that can write to STDOUT.
-  #
-  class Stdout < ::Logging::Appenders::IO
-
-    # call-seq:
-    #    Stdout.new( name = 'stdout' )
-    #    Stdout.new( :layout => layout )
-    #    Stdout.new( name = 'stdout', :level => 'info' )
-    #
-    # Creates a new Stdout Appender. The name 'stdout' will be used unless
-    # another is given. Optionally, a layout can be given for the appender
-    # to use (otherwise a basic appender will be created) and a log level
-    # can be specified.
-    #
-    # Options:
-    #
-    #    :layout   => the layout to use when formatting log events
-    #    :level    => the level at which to log
-    #
-    def initialize( *args )
-      opts = Hash === args.last ? args.pop : {}
-      name = args.empty? ? 'stdout' : args.shift
-
-      opts[:encoding] = STDOUT.external_encoding if STDOUT.respond_to? :external_encoding
-
-      super(name, STDOUT, opts)
-    end
-
-    private
-
-      # @override of ::Logging::Appenders::IO
-      def canonical_write( str )
-        return self if @io.nil?
-        str = str.force_encoding(encoding) if encoding && str.encoding != encoding
-        @io.write str # instead of syswrite
-        self
-      rescue StandardError => err
-        handle_internal_error(err)
-      end
-
-  end  # Stdout
-
-
   # Accessor / Factory for the Stderr appender.
   #
   def self.stderr( *args )
@@ -61,46 +69,4 @@ module Logging::Appenders
     end
     ::Logging::Appenders::Stderr.new(*args)
   end
-
-  # This class provides an Appender that can write to STDERR.
-  #
-  class Stderr < ::Logging::Appenders::IO
-
-    # call-seq:
-    #    Stderr.new( name = 'stderr' )
-    #    Stderr.new( :layout => layout )
-    #    Stderr.new( name = 'stderr', :level => 'warn' )
-    #
-    # Creates a new Stderr Appender. The name 'stderr' will be used unless
-    # another is given. Optionally, a layout can be given for the appender
-    # to use (otherwise a basic appender will be created) and a log level
-    # can be specified.
-    #
-    # Options:
-    #
-    #    :layout   => the layout to use when formatting log events
-    #    :level    => the level at which to log
-    #
-    def initialize( *args )
-      opts = Hash === args.last ? args.pop : {}
-      name = args.empty? ? 'stderr' : args.shift
-
-      opts[:encoding] = STDERR.external_encoding if STDERR.respond_to? :external_encoding
-
-      super(name, STDERR, opts)
-    end
-
-    private
-
-      # @override of ::Logging::Appenders::IO
-      def canonical_write( str )
-        return self if @io.nil?
-        str = str.force_encoding(encoding) if encoding && str.encoding != encoding
-        @io.write str # instead of syswrite
-        self
-      rescue StandardError => err
-        handle_internal_error(err)
-      end
-
-  end  # Stderr
-end  # Logging::Appenders
+end
